bd49b06d448487e1a113c225caeb1ff210becb13
Nicolas Cortes <nicolas.15@live.cl>
2025-08-20 10:40:05 -0400

    conversion_tiempo
    Se agregó la conversión de minutos a segundos y de horas a minutos.

---
+++ conversions/time.py
@@ -0,0 +1,7 @@
+def min_to_sec(min: float) -> float:
+    """Convierte minutos a segundos."""
+    return min * 60
+
+def hour_to_min(hour: float) -> float:
+    """Convierte horas a minutos."""
+    return hour * 60
