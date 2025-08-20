4857009bce3f05156755b96dc57d77e841c9a098
Nicolas Contreras <nmj.contreras@gmail.com>
2025-08-20 10:37:52 -0400

    Se agrega la opcion para la conversion de metros a centimetros en el fichero main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     print("=== Conversor de Unidades ===")
     print("1. Metros a Pies")
     print("2. Kilogramos a Libras")
+    print("5. Metros a Centimetros")
 
     opcion = valid_input("Selecciona una opción: ", int, "Por favor, ingresa un número válido.")
 
@@ -15,6 +16,9 @@ def main():
     elif opcion == 2:
         kilos = valid_input("Ingresa kilogramos: ", float, "Por favor, ingresa un número válido.")
         print(f"{kilos} kg = {kg_to_pounds(kilos):.2f} libras")
+    elif opcion == 5:
+        metros = valid_input("Ingresa metros: ", int, "Por favor, ingresa un número válido.")
+        print(f"{metros} metros = {meters_to_centimeters(metros):.2f} centimetros")
     else:
         print("Opción inválida.")
 
