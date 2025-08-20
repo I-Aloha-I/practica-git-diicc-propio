6251e0573212ca1a59ef4a54d4792fec6bf6ae32
Sebachv96 <sebatronk@hotmail.com>
2025-08-20 10:33:32 -0400

    Se agregan funciones de conversion informatica
    bytes a kilobytes
    kilobytes a megabytes

---
+++ conversions/bytes.py
@@ -0,0 +1,10 @@
+#Bytes a Kilobytes: KB = bytes ÷ 1024
+#Kilobytes a Megabytes: MB = KB ÷ 1024
+
+def bytes_to_kilobytes(bytes: float) -> float:
+    """Convierte bytes a kilobytes"""
+    return bytes / 1024
+
+def kilobytes_to_Megabytes(kilobytes: float) -> float:
+    """Convierte kilobytes a megabytes"""
+    return kilobytes / 1024
